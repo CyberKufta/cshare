ef7c3f7441d72badabe094e4ece84cd36494cd22
Muath S <muath.sharawi@gmail.com>
2014-05-31 08:04:50 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cshare::Application.config.secret_key_base = 'd0ebb7e9b6d7afec5e63c37de2801b37a0bf202ab38da5e2d9a76524067030b9c0f66ec71f3b0c7ad5fd25fd0b14a4ad686f8e23fb2884bd6db971fbf3586710'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cshare::Application.config.session_store :cookie_store, key: '_cshare_session'
