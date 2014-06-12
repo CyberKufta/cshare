9cca1b481ca5a60ca78702edd1188c4633face1e
Muath S <muath.sharawi@gmail.com>
2014-06-12 08:04:51 -0700

    added object error message to home page and shared folder

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ActiveRecord::Base
+  validates_presence_of :image
+  validates_property :format, of: :image, in: ['png', 'jpeg', 'gif'], message: "only valid image files"
   dragonfly_accessor :image
   belongs_to :user
   belongs_to :event
